c5a8e94b5d8028ccc017b9a6b5705a9c9d2eabe3
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-02-18 09:39:14 +0800

    try tailwind

--- src/App.js
+++ src/App.js
@@ -5,29 +5,34 @@ import { motion } from "framer-motion";
 const containerVariants = {
   initial: {
     opacity: 0,
-    y: "-100vh",
+    x: "-100vw",
   },
   animate: {
     opacity: 1,
-    y: 0,
+    x: 0,
     transition: {
       delay: 0.5,
       duration: 1,
     },
   },
+  hover: {
+    scale: 1.1,
+    origin: 0,
+  },
 };
 
 function App() {
   return (
-    <motion.div
-      className="App"
-      variants={containerVariants}
-      initial="initial"
-      animate="animate"
-    >
-      <h1 className="text-5xl font-bold underline m-10 justify-center">
+    <motion.div className="flex justify-center align-middle border h-screen">
+      <motion.h1
+        className="text-5xl font-bold underline m-10 align-middle border-8 h-20"
+        variants={containerVariants}
+        initial="initial"
+        animate="animate"
+        whileHover="hover"
+      >
         Hi My Name is Lester
-      </h1>
+      </motion.h1>
     </motion.div>
   );
 }
