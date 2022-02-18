27bd4c472dc488bab9aaf36b27c5ac5753d0ebdc
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-02-18 09:11:35 +0800

    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import logo from "./lr-logo.svg";
 import "./App.css";
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  initial: {
+    opacity: 0,
+    y: "-100vh",
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.5,
+      duration: 1,
+    },
+  },
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <motion.circle initial={{ pathLength: 0 }} animate={{ pathLength: 1 }} />
-    </div>
+    <motion.div
+      className="App"
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+    >
+      <h1 className="text-5xl font-bold underline m-10 justify-center">
+        Hi My Name is Lester
+      </h1>
+    </motion.div>
   );
 }
 
