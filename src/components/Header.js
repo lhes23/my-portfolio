41a8505081a7d4359e559d89f3990abf1a6d91f4
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-02-18 20:44:36 +0800

    try tailwind nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from "react";
 import logo from "../lr-logo.svg";
+import MainNav from "./MainNav";
 import MenuItems from "./MenuItems";
 
 const Header = () => {
   return (
-    <div className="fixed w-full text-white flex justify-between p-10 items-center backdrop-blur">
-      <div className="">
-        <img src={logo} alt="" className="object-cover h-20 w-50" />
+    <>
+      <MainNav />
+      <div className="fixed w-full text-white flex justify-between p-10 items-center backdrop-blur">
+        <div className="">
+          <img src={logo} alt="" className="object-cover h-20 w-50" />
+        </div>
+        <nav className=" bg-slate-700">
+          <MenuItems />
+        </nav>
       </div>
-      <nav className=" bg-slate-700">
-        <MenuItems />
-      </nav>
-    </div>
+    </>
   );
 };
 
